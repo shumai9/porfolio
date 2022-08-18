169deb724f5045f2acfd45f3afcf5ef314a9da81
shumai9 <ifilyes4@hotmail.com>
2022-08-18 12:44:07 +0100

    Added styles and two controllers 	new file:   app/controllers/about_controller.rb 	new file:   app/controllers/contact_controller.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "pages#welcome"
   get "/projects" => "pages#project_page", as: :projects
+  get "/about" =>"about#about", as: :about
+  get "/contact" =>"contact#contact", as: :contact
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
